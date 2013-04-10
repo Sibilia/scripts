dde88a894221fa9363c8648c6bfca44826515b74
sibilia <sibvilian@gmail.com>
2013-04-10 19:52:51 +0300

    update

--- rotate-j3-extra-logs.sh
+++ rotate-j3-extra-logs.sh
@@ -1,7 +1,7 @@
-#!/bin/bash 
+#!/bin/bash
 #
 # usage:
-# /opt/pmm-bms/sbin/rotate-j3-extra-logs.sh /opt/just 2>&1>> /home/bms-backup/.engines-logs/log-rotate-just-extra.log
+# /opt/pmm-bms/sbin/rotate-j3-extra-logs.sh /opt/just
 #
 
 EngineDir=$1
@@ -51,15 +51,17 @@ find "$LogsDir/$ActualDirName" -type f -name "??????_[0-9]*.log" > /dev/shm/temp
 
 prev_start=
 if [ -e "$ArcDir/$EngineName-on-$HostName-$ActualDirName-0.tar.gz" ]; then
-        prev_start=$(ls -la "$ArcDir/$EngineName-on-$HostName-$ActualDirName-*" |wc -l)
+        prev_start=$(ls -la $ArcDir/$EngineName-on-$HostName-$ActualDirName-* |wc -l)
 else
         prev_start=0
 fi
 
-for i in $(seq $prev_start $(( $(cat /dev/shm/temp.$EngineName.1 |wc -l)/$NumberFile + $prev_start )) ) ; do
+parts=$(( $(cat /dev/shm/temp.$EngineName.1 |wc -l)/$NumberFile))
+for i in $(seq -f %02.0f $prev_start $(( $parts + $prev_start )) ) ; do
         log_files=$(head -n $NumberFile /dev/shm/temp.$EngineName.1)
-        echo "create $(( $i + 1 - $prev_start)) archive file"
+        echo "create $(( $i + 1 - $prev_start))/$((parts + 1))archive file"
         tar --remove-files -czf "$ArcDir/$EngineName-on-$HostName-$ActualDirName-$i.tar.gz" $log_files
+        log "create $(( $i + 1 - $prev_start))/$((parts + 1)) archive file: $EngineName-on-$HostName-$ActualDirName-$i.tar.gz"
 #       rm -f $log_files
         sed "1,$NumberFile d" /dev/shm/temp.$EngineName.1 > /dev/shm/temp.$EngineName.2
         mv /dev/shm/temp.$EngineName.2 /dev/shm/temp.$EngineName.1
